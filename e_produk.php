20dfe16c9f45713ffc85b0ca0a5c61f90d102619
araa094-a11y <sahabatfillah094@gmail.com>
2026-05-12 10:07:41 +0700

    revisi tugas 3

--- e_produk.php
+++ e_produk.php
@@ -296,10 +296,10 @@ if (isset($_POST['update'])) {
     <!-- ======= Footer ======= -->
     <footer id="footer" class="footer">
         <div class="copyright">
-            &copy; Copyright <strong><span>Nama Sistem</span></strong>. All Rights Reserved
+            &copy; Copyright <strong><span>Tiarashop</span></strong>. All Rights Reserved
         </div>
         <div class="credits">
-            Designed by <a href="">Nama Kalian</a>
+            Designed by <a href="">Mutiara</a>
         </div>
     </footer><!-- End Footer -->
 
--- t_produk.php
+++ t_produk.php
@@ -292,10 +292,10 @@ if (isset($_POST['simpan'])) {
     <!-- ======= Footer ======= -->
     <footer id="footer" class="footer">
         <div class="copyright">
-            &copy; Copyright <strong><span></span></strong>. All Rights Reserved
+            &copy; Copyright <strong><span>Tiarashop</span></strong>. All Rights Reserved
         </div>
         <div class="credits">
-            Designed by <a href="">Nama Kalian</a>
+            Designed by <a href="">Mutiara</a>
         </div>
     </footer><!-- End Footer -->
 
